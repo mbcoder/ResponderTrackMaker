8297fb77259c29bce35682e88ffbf2b3539f7225
mbcoder <mbaird@esri.com>
2024-02-23 14:49:35 +0000

    Work in progress

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,12 +17,26 @@
 package com.mycompany.app;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.data.ArcGISFeatureTable;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.QueryParameters;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.Polyline;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.MobileMapPackage;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import java.util.concurrent.ExecutionException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -48,22 +62,134 @@ public class App extends Application {
         Scene scene = new Scene(stackPane);
         stage.setScene(scene);
 
-        // Note: it is not best practice to store API keys in source code.
-        // An API key is required to enable access to services, web maps, and web scenes hosted in ArcGIS Online.
-        // If you haven't already, go to your developer dashboard to get your API key.
-        // Please refer to https://developers.arcgis.com/java/get-started/ for more information
-        String yourApiKey = "YOUR_API_KEY";
-        ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
+        ArcGISMap map = new ArcGISMap(SpatialReferences.getWgs84());
 
         // create a MapView to display the map and add it to the stack pane
         mapView = new MapView();
+        mapView.setMap(map);
         stackPane.getChildren().add(mapView);
 
-        // create an ArcGISMap with an imagery basemap
-        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY);
+        GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+        mapView.getGraphicsOverlays().add(graphicsOverlay);
+        SimpleLineSymbol simpleLineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLUE, 10);
 
-        // display the map by setting the map on the map view
-        mapView.setMap(map);
+        MobileMapPackage mobileMapPackage = new MobileMapPackage("ResponderTracks.mmpk");
+        mobileMapPackage.loadAsync();
+        mobileMapPackage.addDoneLoadingListener(() -> {
+            System.out.println("loaded " + mobileMapPackage.getLoadStatus());
+
+
+            FeatureLayer layer = (FeatureLayer) mobileMapPackage.getMaps().get(0).getOperationalLayers().get(0);
+            System.out.println("name : " + layer.getName());
+
+            ArcGISFeatureTable table = (ArcGISFeatureTable) layer.getFeatureTable();
+
+            QueryParameters queryParameters = new QueryParameters();
+            queryParameters.setWhereClause("1=1");
+
+            var resultFuture = table.queryFeaturesAsync(queryParameters);
+            resultFuture.addDoneListener(()-> {
+                System.out.println("got result");
+                try {
+                    var result = resultFuture.get();
+
+                    for (Feature feature : result) {
+                        System.out.println("adding graphic");
+                        Graphic graphic = new Graphic(feature.getGeometry(), feature.getAttributes(), simpleLineSymbol);
+                        graphicsOverlay.getGraphics().add(graphic);
+
+                    }
+
+                    generateJson(graphicsOverlay);
+
+
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+
+        });
+    }
+
+    private void generateJson(GraphicsOverlay graphicsOverlay) {
+        boolean done = false;
+        boolean gotData;
+        int position = 0;
+
+        while (!done) {
+
+            gotData = false;
+
+            for (Graphic graphic : graphicsOverlay.getGraphics()) {
+                Polyline line = (Polyline) graphic.getGeometry();
+
+                String id = (String) graphic.getAttributes().get("ID");
+                String trackType = (String) graphic.getAttributes().get("TrackType");
+
+
+                try {
+                    Point point = line.getParts().get(0).getPoint(position);
+                    Point latLong = (Point) GeometryEngine.project(point, SpatialReferences.getWgs84());
+
+                    //calculate bearing if not the first point
+                    double heading = 0;
+                    if (position > 1) {
+                        Point lastPoint = line.getParts().get(0).getPoint(position-1);
+                        heading = headingBetweenPoints(point, lastPoint);
+                    }
+
+                    double altitude = 0;
+                    double speed = 0;
+
+                    // set altitude and speed for helicopter
+                    if (id.equals("WS61-4")) {
+                        altitude = 300;
+                        speed = 20;
+                    }
+
+                    // {"geometry":{"x":-122.39063,"y":47.62897,"spatialReference":{"wkid":4326}},"attributes":{"ID":"Responder01","Heading":45.4,"Altitude":0,"Speed":0,"CallSign":"VEH001","TrackType":"VEHICLE"}}
+
+                    System.out.println("{\"geometry\":{\"x\":" + latLong.getX() + ",\"y\":" + latLong.getY() + ",\"spatialReference\":{\"wkid\":4326}}," +
+                        "\"attributes\":{\"ID\":\"" + id + "\"," +
+                        "\"Heading\":" + heading + "," +
+                        "\"Altitude\":" + altitude + "," +
+                        "\"Speed\":" + speed + "," +
+                        "\"CallSign\":\"" + id + "\"," +
+                        "\"TrackType\":\"" + trackType + "\"}}"
+                    );
+                    gotData = true;
+                } catch (Exception e) {};
+
+                position++;
+                if (!gotData) done = true;
+            }
+        }
+    }
+
+    private double headingBetweenPoints(Point point, Point originPoint) {
+
+        //double heading = 0;
+
+        var xDiff = point.getX() - originPoint.getX();
+        var yDiff = point.getY() - originPoint.getY();
+
+        //var distance = GeometryEngine.distanceBetween(originPoint,point);
+
+        var bearing = Math.toDegrees(Math.atan(xDiff / yDiff));
+        // correct if heading South West
+        if (yDiff < 0 && xDiff < 0) {
+            bearing = bearing -180;
+        }
+
+        // correct if heading South East
+        if (yDiff < 0 && xDiff > 0) {
+            bearing = 180 + bearing;
+        }
+        return bearing;
     }
 
     /**
